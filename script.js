dd1e6082a462f5dcb76ece1d0db52b73e86ee715
Samuel Oliveira <samu.prog00@gmail.com>
2023-02-03 16:39:21 -0300

    primeiro-commit

---
+++ script.js
@@ -0,0 +1,3 @@
+var nome = prompt("Qual o seu nome?");
+
+alert ("Seja bem-vindo, " + nome + ". No nosso site te damos todas informações necessárias das aulas, professores, nossas redes sociais e a localização do estabelecimento. Fique a vontade.😀");
